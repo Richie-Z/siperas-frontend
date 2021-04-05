f10fa0c12dd030c38ba898ce3d8e1708abd8f2c4
Richie-Z <zakariarichie@yahoo.com>
2021-04-05 13:47:08 +0700

    create pembayaran view

---
+++ src/views/menu/pembayaran/Index.vue
@@ -0,0 +1,15 @@
+<template>
+  <breadcrumb-vue page="Pembayaran" />
+</template>
+
+<script>
+import BreadcrumbVue from "../../../components/Breadcrumb.vue";
+export default {
+  components: {
+    BreadcrumbVue,
+  },
+};
+</script>
+
+<style>
+</style>
